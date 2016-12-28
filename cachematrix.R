23eef09764f0f5df75f7da39b06efe791fbbc3ab
Dexin Chen <dexinchentum@gmail.com>
2016-12-28 15:58:59 +0100

    program for peer review, JHK uni coursera course R programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions to compute a matrix's inverse with cache functionality
+## 
 
-## Write a short comment describing this function
+## function to create a makeCacheMatrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  x_prime<-NULL
+  set_matrix<-function(y){
+    x<<-y
+    x_prime<<-NULL
+  }
+  get_matrix<-function() x
+  set_inverse<-function(inv) x_prime<<-inv
+  get_inverse<-function() x_prime
+  list(x=x,x_prime=x_prime,get_matrix=get_matrix,set_inverse=set_inverse,
+       get_inverse=get_inverse)
 }
 
 
-## Write a short comment describing this function
+## calculate matrix inverse using func solve()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  x_inv<-x$get_inverse()
+  if(!is.null(x_inv))
+  {
+    message("getting cached data")
+    return(x_inv)
+  }
+  x_inv<-x$get_matrix()
+  x_inv<-solve(x_inv)
+  x$set_inverse(x_inv)
+  x_inv
 }
